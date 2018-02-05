c741d2ee4ac0bdb4514eeccad26a124e57c15b6f
Aaron Harburg <connect@aaronharburg.com>
2018-02-05 17:00:27 -0500

    Added Older Code, fixed bootstrap import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers'
+import App from './containers/App';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+//This reducer is responsible for sending the correct media to the starte
+//It also is responsible for tracking the score
+const updateLevel = (store) => (next) => (action) => {
+  console.log(action.payload);
+  console.log(store.getState())
+next(action);
+}//updateLevel middleware
+
+const createStoreWithMiddleware = applyMiddleware(updateLevel)(createStore);
+////////////////////////middleware
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
+
+
+//store.subscribe(()=>{console.log("it changed", store.getState())})
+/*ReactDOM.render(
+    <App />,
+  document.getElementById('root'));
 registerServiceWorker();
+*/
+//This merely logs the store as it changes
