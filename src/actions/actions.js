06676171186b2cf38663e7f58521af6f3c97356e
Aaron Harburg <connect@aaronharburg.com>
2018-03-16 16:57:55 -0400

    Google sheet actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,67 +1,67 @@
-export const CORRECT = 'CORRECT';
-export const NEXT = 'NEXT';
-export const INCORRECT = 'INCORRECT';
-export const RESET = 'RESET';
-export const CLICK = 'CLICK';
-export const EXPLANATION = 'EXPLANATION';
-export const DEFAULT = 'DEFAULT';
-export const SPREADSHEET_REQUEST = 'SPREADSHEET_REQUEST'
-export const SPREADSHEET_RECEIVED = 'SPREADSHEET_RECEIVED'
-export const SPREADSHEET_FAILED = 'SPREADSHEET_FAILED'
+export const CORRECT = "CORRECT";
+export const NEXT = "NEXT";
+export const INCORRECT = "INCORRECT";
+export const RESET = "RESET";
+export const CLICK = "CLICK";
+export const EXPLANATION = "EXPLANATION";
+export const DEFAULT = "DEFAULT";
+export const SPREADSHEET_REQUEST = "SPREADSHEET_REQUEST";
+export const SPREADSHEET_RECEIVED = "SPREADSHEET_RECEIVED";
+export const SPREADSHEET_FAILED = "SPREADSHEET_FAILED";
 
 //Action Types
 //////////////////////////////////////////////////Action creators
-export function correctResponse(id){
+export function correctResponse(id) {
   return {
     type: CORRECT,
     payload: id,
-    score: 1,
-  }
+    score: 1
+  };
 }
 
-export function nextRound(id){
+export function nextRound(id) {
   return {
     type: NEXT,
-    payload: id,
-  }
+    payload: id
+  };
 }
 
-export function reset(){
+export function reset() {
   return {
     type: RESET,
     score: 0
-  }
+  };
 }
 
-export function incorrectResponse(id, selected, correct){
+export function incorrectResponse(id, selected, correct) {
   return {
     type: INCORRECT,
     payload: id,
     selected: selected,
     correct: correct
-  }
+  };
 }
 /////////////////////////////////////
 
-export function muteButton(currentState){
+export function muteButton(currentState) {
   return {
     type: CLICK,
     payload: currentState
-  }
+  };
 }
 
-export function explainData(move){
+export function explainData(move) {
   return {
     type: EXPLANATION,
     payload: move,
-    pathname: '/explanation'
-  }
+    pathname: "/explanation"
+  };
 }
 
-export function explainDefault(){
-  return{
+export function explainDefault() {
+  return {
     type: DEFAULT
-  }
+  };
 }
 
 ///////////////////////////////Spreadsheets
@@ -69,7 +69,7 @@ export function explainDefault(){
 export function requestSpreadsheet() {
   return {
     type: SPREADSHEET_REQUEST
-  }
+  };
 }
 
 export function receiveSpreadsheet(data) {
@@ -78,7 +78,7 @@ export function receiveSpreadsheet(data) {
     payload: {
       data: data
     }
-  }
+  };
 }
 
 export function receiveSpreadsheetError(error) {
@@ -87,5 +87,32 @@ export function receiveSpreadsheetError(error) {
     payload: {
       error: error
     }
-  }
+  };
 }
+
+let URLS =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSJ1TQJzBcK0o0nvk8HHvYZlOO_n9DClLEib6-lm7S6DH-POSNsTPBARVDqiZkvaKsxjro4b9yQqpGO/pub?gid=0&single=true&output=csv";
+
+// --- API ---
+export function fetchTable() {
+  // Code related to API here. Should just return a promise.
+  // Someting like...
+  return fetch(URLS);
+}
+
+// --- Thunks ---
+export function getSpreadsheetData() {
+  return function(dispatch, getState) {
+    // Tell reducers that you are about to make a request.
+    dispatch(requestSpreadsheet());
+
+    // Make the request, then tell reducers about
+    // whether it succeeded or not.
+    return fetchTable()
+      .then(data => data.text())
+      .then(text => dispatch(receiveSpreadsheet(text)))
+      .catch(error => dispatch(receiveSpreadsheetError(error)));
+  };
+}
+
+//
