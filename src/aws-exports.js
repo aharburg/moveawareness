802c546c28fb9673dfa47cb5f5a160d36b665aec
Aaron Harburg <connect@aaronharburg.com>
2018-03-16 16:58:25 -0400

    aws integrations

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,19 +7,14 @@
 // AWS Mobile Hub Project Constants
 const awsmobile = {
     'aws_app_analytics': 'enable',
-    'aws_cognito_identity_pool_id': 'us-east-1:f7f9feac-bf1b-42ed-aeea-1b49d15c1813',
+    'aws_cognito_identity_pool_id': 'us-east-1:b3cfabad-98ca-43f9-8605-d788f296dafb',
     'aws_cognito_region': 'us-east-1',
-    'aws_content_delivery': 'enable',
-    'aws_content_delivery_bucket': 'moveawareness-hosting-mobilehub-1755283049',
-    'aws_content_delivery_bucket_region': 'us-east-1',
-    'aws_content_delivery_cloudfront': 'enable',
-    'aws_content_delivery_cloudfront_domain': 'd2efpej999v52r.cloudfront.net',
-    'aws_mobile_analytics_app_id': 'e027bf5a135c49649d313f3227d51fb7',
+    'aws_mobile_analytics_app_id': 'dc937fc4bafe449cbf786fb896056de1',
     'aws_mobile_analytics_app_region': 'us-east-1',
-    'aws_project_id': 'be433c20-a652-4c93-b861-0d4411fb4348',
-    'aws_project_name': 'moveawareness',
+    'aws_project_id': 'c8edd3f4-e3d9-4348-86f7-ef614ba4de3c',
+    'aws_project_name': 'MoveAwareness',
     'aws_project_region': 'us-east-1',
-    'aws_resource_name_prefix': 'moveawareness-mobilehub-1755283049',
+    'aws_resource_name_prefix': 'moveawareness-mobilehub-160519728',
 }
 
 export default awsmobile;
