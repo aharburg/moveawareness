c55c7ba2e9e501890f13a20762b0c5ce5a32ec17
Aaron Harburg <connect@aaronharburg.com>
2018-03-16 16:59:41 -0400

    Google sheets component

--- src/containers/Spreadsheet.js
+++ src/containers/Spreadsheet.js
@@ -1,34 +1,11 @@
 // containers/Spreadsheet.js
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { requestSpreadsheet, receiveSpreadsheet, receiveSpreadsheetError } from '../actions/actions'
-let URL = 'https://docs.google.com/spreadsheets/d/1coZh_S_FWxN16BYP5mXowwbOq9gaMQCcdUE8hkWZEsU/edit?usp=sharing'
-
-// --- API ---
-export function fetchTable() {
-  // Code related to API here. Should just return a promise.
-  // Someting like...
-  return fetch(URL)
-}
-
-// --- Thunks ---
-export function getSpreadsheetData() {
-  return function (dispatch, getState) {
-    // Tell reducers that you are about to make a request.
-    dispatch(requestSpreadsheet())
-
-    // Make the request, then tell reducers about
-    // whether it succeeded or not.
-    return fetchTable().then(
-      data => dispatch(receiveSpreadsheet(data)),
-      error => dispatch(receiveSpreadsheetError(error))
-    )
-  }
-}
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { getSpreadsheetData } from "../actions/actions";
 
 class Sheets extends Component {
-  loadSpreadsheet(){
+  loadSpreadsheet() {
     return this.props.getSpreadsheetData();
   }
 
@@ -36,9 +13,9 @@ class Sheets extends Component {
     return (
       <div>
         {this.loadSpreadsheet()}
-          {console.log(this.props.spreadsheetData)}
+        {console.log(this.props.spreadsheetData)}
       </div>
-    )
+    );
   }
 }
 
@@ -47,12 +24,15 @@ function mapStateToProps(state) {
     spreadsheetData: state.spreadsheetData,
     loading: state.loading,
     errorMsg: state.errorMsg
-  }
+  };
 }
 
-function mapDispatchToProps(dispatch){
-  return bindActionCreators({ getSpreadsheetData: getSpreadsheetData }, dispatch);
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { getSpreadsheetData: getSpreadsheetData },
+    dispatch
+  );
 }
 
 //
-export default connect(mapStateToProps, mapDispatchToProps)(Sheets)
+export default connect(mapStateToProps, mapDispatchToProps)(Sheets);
