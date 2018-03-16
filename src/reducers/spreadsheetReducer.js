b1a8db15de062d4348e5406702f5623a526dd6bb
Aaron Harburg <connect@aaronharburg.com>
2018-03-16 17:05:56 -0400

    Reducer for spreadsheets work

--- src/reducers/spreadsheetReducer.js
+++ src/reducers/spreadsheetReducer.js
@@ -2,32 +2,31 @@
 const initialState = {
   spreadsheetData: {},
   loading: false,
-  errorMsg: ''
-}
+  errorMsg: ""
+};
 
 export function spreadsheetReducer(state = initialState, action = {}) {
   switch (action.type) {
-    case 'SPREADSHEET_REQUEST':
+    case "SPREADSHEET_REQUEST":
       return {
         ...state,
         loading: true
-      }
+      };
 
-    case 'SPREADSHEET_RECEIVED':
+    case "SPREADSHEET_RECEIVED":
       return {
         ...state,
         loading: false,
         spreadsheetData: action.payload.data.data,
-        errorMsg: ''
-      }
+        errorMsg: ""
+      };
 
-    case 'SPREADSHEET_FAIL':
+    case "SPREADSHEET_FAIL":
       return {
         loading: false,
         errorMsg: action.payload.error
-      }
+      };
     default:
-        return state;
+      return state;
   }
-
 }
